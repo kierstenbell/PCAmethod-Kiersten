c6a76865850504baefd19cf8891a5deaa0db060e
ecarlson5683 <ecarlson5683@gmail.com>
2024-04-17 16:46:40 -0500

    first commit

---
+++ R/PCAmethod.R
@@ -0,0 +1,26 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+#
+#' @title An interactive principle component analysis function
+#' @description This function allows you to ...
+# @param
+# @keywords
+# @export
+# @examples
+#
+
+hello <- function() {
+  print("Hello, world!")
+}
